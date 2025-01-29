e9fe9a0a112fdfd5fa42b8e29920fe522b5f25ce
root <root@Steven-Thinkpad.>
2025-01-28 20:27:24 -0700

    cpp to cc

--- main.cc
+++ main.cc
@@ -3,6 +3,9 @@
 // Date:        January 27, 2025
 // Description: Main program to run OD.
 
+#include <iostream>
+
 int main() {
-    
+    std::cout << "Hello World!" << std::endl;
+    return 0;
 }
